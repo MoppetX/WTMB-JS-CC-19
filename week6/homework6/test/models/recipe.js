17bec06b52427d426bf8da8dc15419ab5a7a5b0c
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-26 14:52:53 +0100

    test/models/recipe now testing for invalid title

--- week6/homework6/test/models/recipe.js
+++ week6/homework6/test/models/recipe.js
@@ -1,25 +1,20 @@
 import test from 'ava';
 import RecipeModel from '../../models/recipe';
 
+const {
+  recipeModelErrorMsgs: newErrMsgs,
+} = require('../../models/modelErrorMsgs');
+
+const getErrMsgArray = property =>
+  Object.entries(newErrMsgs[property.toUpperCase()]).map(entry => entry[1]);
+
 test.beforeEach(t => {
   t.context = {
     recipe: {
       title: 'Eggs on Toast',
     },
-    errorMsgs: {
-      title: {
-        required: 'Title is required',
-        tooShort: 'Title should be longer than 1 letter',
-        tooLong: 'The maximum characters allowed is 30',
-      },
-    },
   };
 });
-
-const getErrorMsg = (badRecipe, badProperty) => {
-  const error = badRecipe.validateSync();
-  return error.errors[badProperty].message;
-};
 
 test('creating new recipe with valid input', async t => {
   t.plan(4);
@@ -28,7 +23,37 @@ test('creating new recipe with valid input', async t => {
   const error = validRecipe.validateSync();
 
   t.falsy(error);
-  t.true(Array.isArray(validRecipe.versions));
+  t.true(
+    Array.isArray(validRecipe.versions) && validRecipe.versions.length === 0,
+  );
   t.true(Array.isArray(validRecipe.users));
+  console.log(validRecipe.users);
   t.truthy(validRecipe.created);
+});
+
+const testInput = (input, index, t, property, errorMessages) => {
+  // setting a field on the context recipe object to an invalid value
+  t.context.recipe[property] = input;
+  // because errorMessages is an array, we need to keep up in it within our forEach
+  let errorMsgExpected = errorMessages[index];
+
+  // after creating badRecipe, using a mongoose validator func
+  // to catch the error message that would be generated
+  // if badRecipe.save() were called
+  let badRecipe = new RecipeModel(t.context.recipe);
+  const error = badRecipe.validateSync();
+  let errorMsg = error.errors[property].message;
+
+  // compare the returned message to the expected
+  t.is(errorMsg, errorMsgExpected);
+};
+
+test('creating a recipe with invalid title', async t => {
+  const property = 'title';
+  const badInputs = [null, 'e', 'sssssssssssssssssssssssssssssss'];
+  const errorMessages = getErrMsgArray(property);
+
+  badInputs.forEach((input, index) => {
+    testInput(input, index, t, property, errorMessages);
+  });
 });
